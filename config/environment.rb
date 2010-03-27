c706af4fccc170db2d5427b756fee0a2323c1541
sapthagiri <sapthagiri269@gmail.com>
2010-03-27 13:12:09 +0530

    tlsmail

--- config/environment.rb
+++ config/environment.rb
@@ -39,4 +39,5 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
   config.gem "openrain-action_mailer_tls", :lib => "smtp_tls.rb", :source => "http://gems.github.com"
+  config.gem "tlsmail"
 end
