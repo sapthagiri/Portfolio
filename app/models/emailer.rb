0b0c811627cb0e21af09ddd3185984f88bd5e573
sapthagiri <sapthagiri269@gmail.com>
2010-04-06 21:41:37 +0530

    sapthakiri

--- app/models/emailer.rb
+++ app/models/emailer.rb
@@ -2,7 +2,7 @@ class Emailer < ActionMailer::Base
   def contact(name, email, subject, message)
     @name = name
     @subject = subject
-    @recipients = "sapthagiri269@gmail.com"
+    @recipients = "sapthagiri269@gmail.com", "sap@sapthakiri.com"
     @from = email
     @sent_on = Time.now
     @message = message
