a2e342ffe2e6a3ce2eb1df064738eae4e311878d
sapthagiri <sapthagiri269@gmail.com>
2010-04-11 13:18:07 +0530

    minor

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -1,4 +1,24 @@
 class ResumeController < ApplicationController
   def index
   end
+  
+  def doc
+    @filename ="#{RAILS_ROOT}/tmp/resume/sap.doc"
+    send_file(@filename ,
+                :filename      =>  "sapthakiri-resume",
+                :type          =>  'application/msword',
+                :disposition  =>  'attachment',
+                :streaming    =>  'true',
+            :buffer_size  =>  '4096')
+  end
+  
+  def pdf
+    @filename ="#{RAILS_ROOT}/tmp/resume/sap.pdf"
+    send_file(@filename ,
+                :filename      =>  "sapthakiri-resume",
+                :type          =>  'application/pdf',
+                :disposition  =>  'attachment',
+                :streaming    =>  'true',
+            :buffer_size  =>  '4096')
+  end
 end
