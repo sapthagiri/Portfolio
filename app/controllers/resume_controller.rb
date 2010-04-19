83de17e37d551d675e2ede316ca91a5969c03f77
sapthagiri <sapthagiri269@gmail.com>
2010-04-19 20:40:09 +0530

    minor

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -3,12 +3,12 @@ class ResumeController < ApplicationController
   end
   
   def doc
-    @filename ="#{RAILS_ROOT}/tmp/resume/sap-doc.doc"
+    @filename ="#{RAILS_ROOT}/public/resume/sap-doc.doc"
     send_file @filename, :disposition => 'inline'
   end
   
   def pdf
-    @filename ="#{RAILS_ROOT}/tmp/resume/sap-pdf.pdf"
+    @filename ="#{RAILS_ROOT}/public/resume/sap-pdf.pdf"
     send_file @filename, :disposition => 'inline'
   end
 end
