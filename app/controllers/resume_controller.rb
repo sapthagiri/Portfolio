bf545005e9ddf9b217f76938c4a5ff79fa68fb9b
sapthagiri <sapthagiri269@gmail.com>
2011-01-16 14:41:40 +0530

    Added new logo

--- app/controllers/resume_controller.rb
+++ app/controllers/resume_controller.rb
@@ -3,12 +3,12 @@ class ResumeController < ApplicationController
   end
   
   def doc
-    @filename ="#{RAILS_ROOT}/public/resume/SAPTHAKIRI-RESUME.doc"
+    @filename ="#{RAILS_ROOT}/public/resume/Sapthakiri-Resume.doc"
     send_file @filename, :disposition => 'inline'
   end
   
   def pdf
-    @filename ="#{RAILS_ROOT}/public/resume/SAPTHAKIRI-RESUME.pdf"
+    @filename ="#{RAILS_ROOT}/public/resume/Sapthakiri-Resume.pdf"
     send_file @filename, :disposition => 'inline'
   end
 end
